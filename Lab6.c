c83bfc56b040a7b5b1f86d12e8873e7a92138ffd
Jacob Parmer <jacob.parmer@adtran.com>
2020-02-24 07:31:49 -0600

    Updated Lab6 with in-lab changes

--- Lab6.c
+++ Lab6.c
@@ -14,6 +14,7 @@ uint16_t count_decimal;
 uint16_t key_selected;
 bool timer_enabled = false;
 bool action_taken = false;
+uint16_t test, test2;
 
 /*---------------------------------------------------*/
 /* Initialize GPIO pins used in the program */
@@ -52,7 +53,7 @@ void interruptSetup() {
 	NVIC_SetPriority(EXTI1_IRQn, 1); // sets priority of EXTI1
 	
 	NVIC_EnableIRQ(TIM10_IRQn); // enable external interrupt TIM10
-	NVIC_SetPriority(TIM10_IRQn, 1); // sets priority of TIM10
+	NVIC_SetPriority(TIM10_IRQn, 2); // sets priority of TIM10
 	
 	__enable_irq(); // global interrupt enable
 
@@ -118,6 +119,7 @@ void scan() {
 	uint16_t row_selected;
 	int i;
 	
+	test = GPIOB->IDR & 0x000F;
 	GPIOB->MODER &= ~(0x0000FFFF); /* General purpose input mode for PB[0:7] */
 	GPIOB->MODER |= 0x00005500; /* Output mode for PB[4:7] */
 	
@@ -125,6 +127,7 @@ void scan() {
 	GPIOB->PUPDR &= 0x000000FF;
 	GPIOB->ODR = 0xFF7F;
 	
+	test2 = GPIOB->IDR & 0x000F;
 	for (i=0; i<4; i++) { // loops through 4 columns from right to left
 		
 		delay(5);
@@ -153,6 +156,29 @@ void scan() {
 		if (keypress_found == true) {
 			key_selected = keypad[row_selected][i];
 			keypress_found = false;
+		
+			if (key_selected == 0x0000) { // checks for keypress of key 0
+				if (timer_enabled == true) {
+					timer_enabled = false;
+					TIM10->CR1 &= ~TIM_CR1_CEN; // disables timer clock
+					GPIOC->ODR &= 0x00;
+					GPIOC->ODR |= (count_integer | count_decimal); // Outputs count to pins PC[0:7].
+				} else {
+					timer_enabled = true;
+					TIM10->CR1 |= TIM_CR1_CEN; // enables timer clock
+					GPIOC->ODR &= 0x00;
+					GPIOC->ODR |= (count_integer | count_decimal); // Outputs count to pins PC[0:7].
+				}
+			}
+			
+			if (key_selected == 0x0001) { // checks for keypress of key 1
+				if (timer_enabled == false) {
+					count_integer = 0x0000;
+					count_decimal = 0x0000;
+					GPIOC->ODR &= 0x00;
+					GPIOC->ODR |= (count_integer | count_decimal); // Outputs count to pins PC[0:7].
+				}
+			}			
 		}
 			
 		GPIOB->ODR = GPIOB->ODR>>1;
@@ -166,7 +192,7 @@ void scan() {
 void EXTI1_IRQHandler(void) {
 
 	scan();
-	delay(100); // delay to prevent retriggering interrupt due to bouncing
+	delay(5000); // delay to prevent retriggering interrupt due to bouncing
 	
 	pinSetup();
 	
@@ -180,6 +206,8 @@ void EXTI1_IRQHandler(void) {
 void TIM10_IRQHandler(void) {
 	
 	counter();
+	GPIOC->ODR &= 0x00;
+	GPIOC->ODR |= (count_integer | count_decimal); // Outputs count to pins PC[0:7].
 	TIM10->SR &= ~TIM_SR_UIF;
 	EXTI->PR |= 0x000002; // clears interrupt pending bits
 	NVIC_ClearPendingIRQ(TIM10_IRQn);
@@ -206,27 +234,6 @@ int main(void) {
 	
  /* Endless loop */
 	while (1) {
-		
-		if (key_selected == 0x0000 & action_taken == false) { // checks for keypress of key 0
-				if (timer_enabled == true) {
-					timer_enabled = false;
-					action_taken = true;
-					TIM10->CR1 &= ~TIM_CR1_CEN; // disables timer clock
-				} else {
-					timer_enabled = true;
-					action_taken = true;
-					TIM10->CR1 |= TIM_CR1_CEN; // enables timer clock
-				}
-		}
-		
-		if (key_selected == 0x0001 & action_taken == false) { // checks for keypress of key 1
-			if (timer_enabled == false) {
-				count_integer = 0x0000;
-				count_decimal = 0x0000;
-				action_taken = true;
-			}
-		}
-		GPIOC->ODR &= 0x00;
-		GPIOC->ODR |= (count_integer | count_decimal); // Outputs count to pins PC[0:7].
+
 	} /* repeat forever */
 }
